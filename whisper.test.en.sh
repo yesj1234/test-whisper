faf077d523fcf3e92154ac03e6e8df93a70db219
jack <juesc007@gmail.com>
2024-01-30 02:31:35 +0000

    fix arguments

--- whisper.test.en.sh
+++ whisper.test.en.sh
@@ -79,10 +79,10 @@
 # --metric wer \
 # --split test 
 
-# covost2
+# covost2. # lang config name for covost2 is like source_target for example, en_ja or en_zh-CN. for audio transcription they are all the same. 
 python3 whisper.test.py \
 --model openai/whisper-large-v2 \
---lang en \
+--lang en_ja \
 --language english \
 --load_script /home/ubuntu/test_whisper/load_scripts/english/my_covost2.py \
 --dataset_name covost2 \
