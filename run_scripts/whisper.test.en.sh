b68273ee37e311220ee19449c2654f78b2b3ff86
jack <juesc007@gmail.com>
2024-01-31 01:08:42 +0000

    update run scripts

--- run_scripts/whisper.test.en.sh
+++ run_scripts/whisper.test.en.sh
@@ -3,89 +3,94 @@
 
 # get the huggingface token from huggingface.co
 # export HF_TOKEN='' 
-
+export WHISPER_TEST=/home/ubuntu/test_whisper/whisper.test.py 
+export MODEL=openai/whisper-large-v2
+export LOAD_SCRIPT_BASE=/home/ubuntu/test_whisper/load_scripts/english
+export LANG=en
+export LANGUAGE=english
+export METRIC=cer
 #libry 
-# python3 whisper.test.py \
-# --model openai/whisper-large-v2 \
-# --lang en \
-# --language english \
-# --load_script /home/ubuntu/test_whisper/load_scripts/english/my_libri.py \
-# --dataset_name libri \
-# --metric wer \
-# --split test.clean
+python3 $WHISPER_TEST \
+--model $MODEL \
+--lang $LANG \
+--language $LANGUAGE \
+--load_script $LOAD_SCRIPT_BASE/my_libri.py \
+--dataset_name libri \
+--metric $METRIC \
+--split test.clean
 
 #cv5
-# python3 whisper.test.py \
-# --model openai/whisper-large-v2 \
-# --lang en \
-# --language english \
-# --load_script /home/ubuntu/test_whisper/load_scripts/english/my_cv5.py \
-# --dataset_name cv5 \
-# --metric wer 
+python3 $WHISPER_TEST \
+--model $MODEL \
+--lang $LANG \
+--language $LANGUAGE \
+--load_script $LOAD_SCRIPT_BASE/my_cv5.py \
+--dataset_name cv5 \
+--metric $METRIC 
 
 #cv9
-# python3 whisper.test.py \
-# --model openai/whisper-large-v2 \
-# --lang en \
-# --language english \
-# --load_script /home/ubuntu/test_whisper/load_scripts/english/my_cv9.py \
-# --dataset_name cv9 \
-# --metric wer 
+python3 $WHISPER_TEST \
+--model $MODEL \
+--lang $LANG \
+--language $LANGUAGE \
+--load_script $LOAD_SCRIPT_BASE/my_cv9.py \
+--dataset_name cv9 \
+--metric $METRIC 
 
 #ihm
-# python3 whisper.test.py \
-# --model openai/whisper-large-v2 \
-# --lang en \
-# --language english \
-# --load_script /home/ubuntu/test_whisper/load_scripts/english/my_ihm.py \
-# --dataset_name ihm \
-# --metric wer 
+python3 $WHISPER_TEST \
+--model $MODEL \
+--lang $LANG \
+--language $LANGUAGE \
+--load_script $LOAD_SCRIPT_BASE/my_ihm.py \
+--dataset_name ihm \
+--metric $METRIC 
 
 #sdm
-# python3 whisper.test.py \
-# --model openai/whisper-large-v2 \
-# --lang en \
-# --language english \
-# --load_script /home/ubuntu/test_whisper/load_scripts/english/my_sdm.py \
-# --dataset_name sdm \
-# --metric wer 
+python3 $WHISPER_TEST \
+--model $MODEL \
+--lang $LANG \
+--language $LANGUAGE \
+--load_script $LOAD_SCRIPT_BASE/my_sdm.py \
+--dataset_name sdm \
+--metric $METRIC 
 
 # #ted
-# python3 whisper.test.py \
-# --model openai/whisper-large-v2 \
-# --lang en \
-# --language english \
-# --load_script /home/ubuntu/test_whisper/load_scripts/english/my_ted.py \
-# --dataset_name ted \
-# --metric wer 
+python3 $WHISPER_TEST \
+--model $MODEL \
+--lang $LANG \
+--language $LANGUAGE \
+--load_script $LOAD_SCRIPT_BASE/my_ted.py \
+--dataset_name ted \
+--metric $METRIC 
 
 #fleurs 
-# python3 whisper.test.py \
-# --model openai/whisper-large-v2 \
-# --lang en_us \
-# --language english \
-# --load_script google/fleurs \
-# --dataset_name fleurs \
-# --metric wer \
-# --split test 
+python3 $WHISPER_TEST \
+--model $MODEL \
+--lang en_us \
+--language $LANGUAGE \
+--load_script google/fleurs \
+--dataset_name fleurs \
+--metric $METRIC \
+--split test 
 
 # vox
-# python3 whisper.test.py \
-# --model openai/whisper-large-v2 \
-# --lang en \
-# --language english \
-# --load_script /home/ubuntu/test_whisper/load_scripts/english/my_vox.py \
-# --dataset_name vox \
-# --metric wer \
-# --split test 
+python3 $WHISPER_TEST \
+--model $MODEL \
+--lang $LANG \
+--language $LANGUAGE \
+--load_script $LOAD_SCRIPT_BASE/my_vox.py \
+--dataset_name vox \
+--metric $METRIC \
+--split test 
 
 # covost2. 
-python3 whisper.test.py \
---model openai/whisper-large-v2 \
---lang en \
---language english \
---load_script /home/ubuntu/test_whisper/load_scripts/english/my_covost2.py \
+python3 $WHISPER_TEST \
+--model $MODEL \
+--lang $LANG \
+--language $LANGUAGE \
+--load_script $LOAD_SCRIPT_BASE/my_covost2.py \
 --dataset_name covost2 \
---metric wer \
+--metric $METRIC \
 --split test \
 --data_dir /home/ubuntu/covost/
--- run_scripts/whisperx.test.en.sh
+++ run_scripts/whisperx.test.en.sh
@@ -4,80 +4,80 @@
 # get the huggingface token from huggingface.co
 # export HF_TOKEN='' 
 export WHISPERX_TEST=/home/ubuntu/test_whisper/whisperx.test.py 
-#libry 
-# python3 $WHISPERX_TEST \
-# --model large-v2 \
-# --lang en \
-# --language english \
-# --load_script /home/ubuntu/test_whisper/load_scripts/english/my_libri.py \
-# --dataset_name libri \
-# --metric wer \
-# --split test.clean
+#libri
+python3 $WHISPERX_TEST \
+--model large-v2 \
+--lang en \
+--language english \
+--load_script /home/ubuntu/test_whisper/load_scripts/english/my_libri.py \
+--dataset_name libri \
+--metric wer \
+--split test.clean
 
 #cv5
-# python3 $WHISPERX_TEST \
-# --model large-v2 \
-# --lang en \
-# --language english \
-# --load_script /home/ubuntu/test_whisper/load_scripts/english/my_cv5.py \
-# --dataset_name cv5 \
-# --metric wer 
+python3 $WHISPERX_TEST \
+--model large-v2 \
+--lang en \
+--language english \
+--load_script /home/ubuntu/test_whisper/load_scripts/english/my_cv5.py \
+--dataset_name cv5 \
+--metric wer 
 
 #cv9
-# python3 $WHISPERX_TEST \
-# --model large-v2 \
-# --lang en \
-# --language english \
-# --load_script /home/ubuntu/test_whisper/load_scripts/english/my_cv9.py \
-# --dataset_name cv9 \
-# --metric wer 
+python3 $WHISPERX_TEST \
+--model large-v2 \
+--lang en \
+--language english \
+--load_script /home/ubuntu/test_whisper/load_scripts/english/my_cv9.py \
+--dataset_name cv9 \
+--metric wer 
 
 #ihm
-# python3 $WHISPERX_TEST \
-# --model large-v2 \
-# --lang en \
-# --language english \
-# --load_script /home/ubuntu/test_whisper/load_scripts/english/my_ihm.py \
-# --dataset_name ihm \
-# --metric wer 
+python3 $WHISPERX_TEST \
+--model large-v2 \
+--lang en \
+--language english \
+--load_script /home/ubuntu/test_whisper/load_scripts/english/my_ihm.py \
+--dataset_name ihm \
+--metric wer 
 
 #sdm
-# python3 $WHISPERX_TEST \
-# --model large-v2 \
-# --lang en \
-# --language english \
-# --load_script /home/ubuntu/test_whisper/load_scripts/english/my_sdm.py \
-# --dataset_name sdm \
-# --metric wer 
+python3 $WHISPERX_TEST \
+--model large-v2 \
+--lang en \
+--language english \
+--load_script /home/ubuntu/test_whisper/load_scripts/english/my_sdm.py \
+--dataset_name sdm \
+--metric wer 
 
 # #ted
-# python3 $WHISPERX_TEST \
-# --model large-v2 \
-# --lang en \
-# --language english \
-# --load_script /home/ubuntu/test_whisper/load_scripts/english/my_ted.py \
-# --dataset_name ted \
-# --metric wer 
+python3 $WHISPERX_TEST \
+--model large-v2 \
+--lang en \
+--language english \
+--load_script /home/ubuntu/test_whisper/load_scripts/english/my_ted.py \
+--dataset_name ted \
+--metric wer 
 
 #fleurs 
-# python3 $WHISPERX_TEST \
-# --model large-v2 \
-# --lang en_us \
-# --language english \
-# --load_script google/fleurs \
-# --dataset_name fleurs \
-# --metric wer \
-# --split test 
+python3 $WHISPERX_TEST \
+--model large-v2 \
+--lang en_us \
+--language english \
+--load_script google/fleurs \
+--dataset_name fleurs \
+--metric wer \
+--split test 
 
 # vox
-# python3 $WHISPERX_TEST \
-# --model large-v2 \
-# --lang en \
-# --language english \
-# --load_script /home/ubuntu/test_whisper/load_scripts/english/my_vox.py \
-# --dataset_name vox \
-# --metric wer \
-# --split test 
+python3 $WHISPERX_TEST \
+--model large-v2 \
+--lang en \
+--language english \
+--load_script /home/ubuntu/test_whisper/load_scripts/english/my_vox.py \
+--dataset_name vox \
+--metric wer \
+--split test 
 
 # covost2. 
 python3 $WHISPERX_TEST \
