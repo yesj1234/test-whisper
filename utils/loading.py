4d39677c933596779c4cfbfaf27f5c59f30d7c63
jack <juesc007@gmail.com>
2024-02-05 04:53:40 +0000

    update loading.py

--- utils/loading.py
+++ utils/loading.py
@@ -4,6 +4,10 @@ class DataLoader:
     def __init__(self):
         pass 
     
+    def _filter_ted(example):
+        # filter ted samples by text containing word "ignore time segment in scoring"
+        return example["text"] != "ignore_time_segment_in_scoring"
+    
     def load(self, dataset_name, **kwargs):
         load_script = kwargs.pop("load_script")
         lang = kwargs.pop("lang")
@@ -11,21 +15,33 @@ class DataLoader:
         if dataset_name == "cv5":
             token=os.environ["HF_TOKEN"]
             return load_dataset(load_script, lang, token=token, trust_remote_code=True)
+        
         if dataset_name == "cv9":
             token=os.environ["HF_TOKEN"]
             return load_dataset(load_script, lang, token=token, trust_remote_code=True)
+        
         if dataset_name == "ihm":
             return load_dataset(load_script, 'ihm', trust_remote_code=True)
+        
         if dataset_name == "sdm":
             return load_dataset(load_script, 'sdm', trust_remote_code=True)
+        
         if dataset_name == "libri":
             return load_dataset(load_script, split=split, trust_remote_code=True)
+        
         if dataset_name == "ted":
-            return load_dataset(load_script, 'release1', trust_remote_code=True)
+            ds = load_dataset(load_script, 'release3', split=split, trust_remote_code=True)
+            ds = ds.filter(self._filter_ted)
+            return ds            
+            
         if dataset_name == "fleurs":
+            if lang == "en":
+                lang = "en_us"
             return load_dataset(load_script, lang, split=split, trust_remote_code=True)
+        
         if dataset_name == "vox":
             return load_dataset(load_script, lang, trust_remote_code = True)
+        
         if dataset_name == "covost2":
             data_dir = kwargs.pop("data_dir", "")
             return load_dataset(load_script, lang, split=split, data_dir=data_dir, trust_remote_code=True)
