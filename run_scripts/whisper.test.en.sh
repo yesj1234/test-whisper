7c186312d90acc16f757c0637caa9c64c136a257
yesj1234 <86505279+yesj1234@users.noreply.github.com>
2024-02-13 18:03:11 +0900

    Update whisper.test.en.sh

--- run_scripts/whisper.test.en.sh
+++ run_scripts/whisper.test.en.sh
@@ -4,88 +4,97 @@
 # get the huggingface token from huggingface.co
 # export HF_TOKEN='' 
 export WHISPER_TEST=/home/ubuntu/test_whisper/whisper.test.py 
-export MODEL=openai/whisper-large-v3
+export MODEL=openai/whisper-large-v2
 export LOAD_SCRIPT_BASE=/home/ubuntu/test_whisper/load_scripts/english
 export LANG=en
 export LANGUAGE=english
-export METRIC=cer
+export METRIC=wer
+export MAX_SIZE=10000
 #libry 
-python3 $WHISPER_TEST \
+python3 -W ignore $WHISPER_TEST \
 --model $MODEL \
 --lang $LANG \
 --language $LANGUAGE \
 --load_script $LOAD_SCRIPT_BASE/my_libri.py \
 --dataset_name libri \
 --metric $METRIC \
---split test.clean 
+--split test.clean \
+--max_size $MAX_SIZE
 
-#cv5
-python3 $WHISPER_TEST \
+# #cv5
+python3 -W ignore $WHISPER_TEST \
 --model $MODEL \
 --lang $LANG \
 --language $LANGUAGE \
 --load_script $LOAD_SCRIPT_BASE/my_cv5.py \
 --dataset_name cv5 \
---metric $METRIC 
+--metric $METRIC \
+--max_size $MAX_SIZE
 
-#cv9
-python3 $WHISPER_TEST \
+# #cv9
+python3 -W ignore $WHISPER_TEST \
 --model $MODEL \
 --lang $LANG \
 --language $LANGUAGE \
 --load_script $LOAD_SCRIPT_BASE/my_cv9.py \
 --dataset_name cv9 \
---metric $METRIC 
+--metric $METRIC \
+--max_size $MAX_SIZE
 
 #ihm
-python3 $WHISPER_TEST \
+python3 -W ignore $WHISPER_TEST \
 --model $MODEL \
 --lang $LANG \
 --language $LANGUAGE \
 --load_script $LOAD_SCRIPT_BASE/my_ihm.py \
 --dataset_name ihm \
---metric $METRIC 
+--metric $METRIC \
+--max_size $MAX_SIZE
 
 #sdm
-python3 $WHISPER_TEST \
+python3 -W ignore $WHISPER_TEST \
 --model $MODEL \
 --lang $LANG \
 --language $LANGUAGE \
 --load_script $LOAD_SCRIPT_BASE/my_sdm.py \
 --dataset_name sdm \
---metric $METRIC 
+--metric $METRIC \
+--max_size $MAX_SIZE
 
 # #ted
-python3 $WHISPER_TEST \
+python3 -W ignore $WHISPER_TEST \
 --model $MODEL \
 --lang $LANG \
 --language $LANGUAGE \
 --load_script $LOAD_SCRIPT_BASE/my_ted.py \
 --dataset_name ted \
---metric $METRIC 
+--metric $METRIC \
+--max_size $MAX_SIZE
 
 #fleurs 
-python3 $WHISPER_TEST \
+python3 -W ignore $WHISPER_TEST \
 --model $MODEL \
 --lang en_us \
 --language $LANGUAGE \
 --load_script google/fleurs \
 --dataset_name fleurs \
 --metric $METRIC \
---split test 
+--split test \
+--max_size $MAX_SIZE
 
 # vox
-python3 $WHISPER_TEST \
+python3 -W ignore $WHISPER_TEST \
 --model $MODEL \
 --lang $LANG \
 --language $LANGUAGE \
 --load_script $LOAD_SCRIPT_BASE/my_vox.py \
 --dataset_name vox \
 --metric $METRIC \
---split test 
+--split test \
+--max_size $MAX_SIZE
 
 # covost2. 
-python3 $WHISPER_TEST \
+python3 -W ignore $WHISPER_TEST \
 --model $MODEL \
 --lang $LANG \
 --language $LANGUAGE \
@@ -93,4 +102,5 @@ python3 $WHISPER_TEST \
 --dataset_name covost2 \
 --metric $METRIC \
 --split test \
---data_dir /home/ubuntu/covost_en/en/en/
+--data_dir /home/ubuntu/covost_en/en/en/ \
+--max_size $MAX_SIZE
