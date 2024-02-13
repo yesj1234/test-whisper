3c4f461e860088a9c0d8943b59361109d5464e54
yesj1234 <86505279+yesj1234@users.noreply.github.com>
2024-02-13 18:02:58 +0900

    Update whisperx.test.en.sh

--- run_scripts/whisperx.test.en.sh
+++ run_scripts/whisperx.test.en.sh
@@ -4,88 +4,105 @@
 # get the huggingface token from huggingface.co
 # export HF_TOKEN='' 
 export WHISPERX_TEST=/home/ubuntu/test_whisper/whisperx.test.py 
+export MODEL=large-v2
+export LOAD_SCRIPT_BASE=/home/ubuntu/test_whisper/load_scripts/english
+export LANG=en
+export LANGUAGE=english
+export METRIC=wer
+export MAX_SIZE=10000
+export SPLIT=test
 #libri
-python3 $WHISPERX_TEST \
---model large-v2 \
---lang en \
---language english \
---load_script /home/ubuntu/test_whisper/load_scripts/english/my_libri.py \
+python3 -W ignore $WHISPERX_TEST \
+--model $MODEL \
+--lang $LANG \
+--language $LANGUAGE \
+--load_script $LOAD_SCRIPT_BASE/my_libri.py \
 --dataset_name libri \
---metric wer \
---split test.clean
+--metric $METRIC \
+--split test.clean \
+--max_size $MAX_SIZE
 
-#cv5
-python3 $WHISPERX_TEST \
---model large-v2 \
---lang en \
---language english \
---load_script /home/ubuntu/test_whisper/load_scripts/english/my_cv5.py \
+# #cv5
+python3 -W ignore $WHISPERX_TEST \
+--model $MODEL \
+--lang $LANG \
+--language $LANGUAGE \
+--load_script $LOAD_SCRIPT_BASE/my_cv5.py \
 --dataset_name cv5 \
---metric wer 
+--metric $METRIC \
+--max_size $MAX_SIZE
 
 #cv9
-python3 $WHISPERX_TEST \
---model large-v2 \
---lang en \
---language english \
---load_script /home/ubuntu/test_whisper/load_scripts/english/my_cv9.py \
+python3 -W ignore $WHISPERX_TEST \
+--model $MODEL \
+--lang $LANG \
+--language $LANGUAGE \
+--load_script $LOAD_SCRIPT_BASE/my_cv9.py \
 --dataset_name cv9 \
---metric wer 
+--metric $METRIC \
+--max_size $MAX_SIZE
 
 #ihm
-python3 $WHISPERX_TEST \
---model large-v2 \
---lang en \
---language english \
---load_script /home/ubuntu/test_whisper/load_scripts/english/my_ihm.py \
+python3 -W ignore $WHISPERX_TEST \
+--model $MODEL \
+--lang $LANG \
+--language $LANGUAGE \
+--load_script $LOAD_SCRIPT_BASE/my_ihm.py \
 --dataset_name ihm \
---metric wer 
+--metric $METRIC \
+--max_size $MAX_SIZE
 
 #sdm
-python3 $WHISPERX_TEST \
---model large-v2 \
---lang en \
---language english \
---load_script /home/ubuntu/test_whisper/load_scripts/english/my_sdm.py \
+python3 -W ignore $WHISPERX_TEST \
+--model $MODEL \
+--lang $LANG \
+--language $LANGUAGE \
+--load_script $LOAD_SCRIPT_BASE/my_sdm.py \
 --dataset_name sdm \
---metric wer 
+--metric $METRIC \
+--max_size $MAX_SIZE
 
-# #ted
-python3 $WHISPERX_TEST \
---model large-v2 \
---lang en \
---language english \
---load_script /home/ubuntu/test_whisper/load_scripts/english/my_ted.py \
+#ted
+python3 -W ignore $WHISPERX_TEST \
+--model $MODEL \
+--lang $LANG \
+--language $LANGUAGE \
+--load_script $LOAD_SCRIPT_BASE/my_ted.py \
 --dataset_name ted \
---metric wer 
+--split $SPLIT \
+--metric $METRIC \
+--max_size $MAX_SIZE
 
 #fleurs 
-python3 $WHISPERX_TEST \
---model large-v2 \
---lang en \
---language english \
+python3 -W ignore $WHISPERX_TEST \
+--model $MODEL \
+--lang $LANG \
+--language $LANGUAGE \
 --load_script google/fleurs \
 --dataset_name fleurs \
---metric wer \
---split test 
+--metric $METRIC \
+--split $SPLIT \
+--max_size $MAX_SIZE
 
 # vox
-python3 $WHISPERX_TEST \
---model large-v2 \
---lang en \
---language english \
---load_script /home/ubuntu/test_whisper/load_scripts/english/my_vox.py \
+python3 -W ignore $WHISPERX_TEST \
+--model $MODEL \
+--lang $LANG \
+--language $LANGUAGE \
+--load_script $LOAD_SCRIPT_BASE/my_vox.py \
 --dataset_name vox \
---metric wer \
---split test 
+--metric $METRIC \
+--split $SPLIT \
+--max_size $MAX_SIZE
 
 # covost2. 
-python3 $WHISPERX_TEST \
---model large-v2 \
---lang en \
---language english \
---load_script /home/ubuntu/test_whisper/load_scripts/english/my_covost2.py \
+python3 -W ignore $WHISPERX_TEST \
+--model $MODEL \
+--lang $LANG \
+--language $LANGUAGE \
+--load_script $LOAD_SCRIPT_BASE/my_covost2.py \
 --dataset_name covost2 \
---metric wer \
---split test \
---data_dir /home/ubuntu/covost/
+--metric $METRIC \
+--split $SPLIT \
+--data_dir /home/ubuntu/covost_en/en/en/ \
+--max_size $MAX_SIZE
